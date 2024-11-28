beaea8a94f59c4a30d663e2fc227df678db5de8f
Amane <3319682713@qq.com>
2024-11-28 23:30:25 +0800

    update StrHash template

--- Algorithm-Templates/StrHash.cpp
+++ Algorithm-Templates/StrHash.cpp
@@ -14,5 +14,21 @@ int findPrime(int n) {
 }
 
 std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
-const int P = findPrime(rng() % 900'000'000 + 900'000'000), 
-base = uniform_int_distribution<>(8e8, 9e8)(rng);
+const int P = findPrime(rng() % 900'000'000 + 900'000'000),
+base = std::uniform_int_distribution<int>(8e8, 9e8)(rng);
+
+const int N = 1e5 + 10;
+ll p[N],h[N];
+
+void init(std::string s) {
+    int n = s.size();
+    p[0] = 1;
+    for (int i = 1; i <= n; i++) {
+        h[i] = (h[i - 1] * base + s[i]) % P;
+        p[i] = p[i - 1] * base % P;
+    }
+}
+
+ll get(int l,int r) {
+    return (h[r] - (h[l - 1] * p[r - l + 1]) % P + P) % P;
+}
