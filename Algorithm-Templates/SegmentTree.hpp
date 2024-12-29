24c91a767cbee1b5510a0d82731912256b93748c
Amane <3319682713@qq.com>
2024-12-29 13:33:05 +0800

    update SegmentTree template

--- Algorithm-Templates/SegmentTree.hpp
+++ Algorithm-Templates/SegmentTree.hpp
@@ -1,62 +1,55 @@
-const int N = 1e5 + 10;
-int a[N],n;
-
-#define sum(x) d[x].sum
-#define len(x) d[x].len
-#define add(x) d[x].add
-struct Node{
-    ll sum;
-    int len;
-    ll add;
-}d[N << 2];
-
-void tag(int cur,int val) {
-    sum(cur) += 1LL * val * len(cur);
-    add(cur) += val;
-}
-
-void pushDown(int cur) {
-    if (add(cur)) {
-        tag(ls(cur),add(cur));
-        tag(rs(cur),add(cur));
-        add(cur) = 0;
+template <class Info>
+struct SegmentTree {
+    int n;
+    vector<Info> info;
+    SegmentTree(): n(0) {}
+    SegmentTree(int n_, Info v_ = Info()) {
+        init(n_, v_);
     }
-}
-
-void pushUp(int cur) {
-    sum(cur) = sum(ls(cur)) + sum(rs(cur));
-}
-
-void build(int cur = 1,int l = 1,int r = n) {
-    len(cur) = r - l + 1;
-    if (l == r) {
-        sum(cur) = a[l];
-        return;
+    void init(int n_, Info v_ = Info()) {
+        init(vector<Info>(n_ + 1, v_));
     }
-    int mid = l + r >> 1;
-    build(ls(cur),l,mid);
-    build(rs(cur),mid + 1,r);
-    pushUp(cur);
-}
-
-void update(int ql,int qr,int val,int cur = 1,int l = 1,int r = n) {
-    if (ql <= l && qr >= r) {
-        tag(cur,val);
-        return;
+    template <class T>
+    void init(vector<T> init_) {
+        n = init_.size() - 1;
+        info.assign(n << 2, Info());
+        function<void(int, int, int)> build = [&](int id, int l, int r) {
+            if (l == r) {
+                info[id] = init_[l];
+                return;
+            }
+            int mid = l + r >> 1;
+            build(ls(id), l, mid);
+            build(rs(id), mid + 1, r);
+            pushUp(id);
+        };
+        build(1, 1, n);
     }
-    pushDown(cur);
-    int mid = l + r >> 1;
-    if (ql <= mid) update(ql,qr,val,ls(cur),l,mid);
-    if (qr > mid) update(ql,qr,val,rs(cur),mid + 1,r);
-    pushUp(cur);
-}
-
-ll query(int ql,int qr,int cur = 1,int l = 1,int r = n) {
-    if (ql <= l && qr >= r) return sum(cur);
-    pushDown(cur);
-    int mid = l + r >> 1;
-    ll res = 0;
-    if (ql <= mid) res += query(ql,qr,ls(cur),l,mid);
-    if (qr > mid) res += query(ql,qr,rs(cur),mid + 1,r);
-    return res;
-}
+    void pushUp(int id) {
+        info[id] = info[ls(id)] + info[rs(id)];
+    }
+    void modify(int id, int l, int r, int x, const Info &v) {
+        if (l == r) {
+            info[id] = v;
+            return;
+        }
+        int mid = l + r >> 1;
+        if (x <= mid) modify(ls(id), l, mid, x, v);
+        else modify(rs(id), mid + 1, r, x, v);
+        pushUp(id);
+    }
+    void modify(int x, const Info &v) {
+        modify(1, 1, n, x, v);
+    }
+    Info query(int id, int l, int r, int x, int y) {
+        if (x > r || y < l) return Info();
+        if (x <= l && y >= r) {
+            return info[id];
+        }
+        int mid = l + r >> 1;
+        return query(ls(id), l, mid, x, y) + query(rs(id), mid + 1, r, x, y);
+    }
+    Info query(int l, int r) {
+        return query(1, 1, n, l, r);
+    }
+};
