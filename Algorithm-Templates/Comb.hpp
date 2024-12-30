99d599d40d588a4af5eae2087f8d5adfbec8f49d
Amane <3319682713@qq.com>
2024-12-30 19:10:21 +0800

    update Comb template

--- Algorithm-Templates/Comb.hpp
+++ Algorithm-Templates/Comb.hpp
@@ -1,26 +1,21 @@
-const int MAXN = 1e5 + 10, mod = 1e9 + 7;
-ll fact[MAXN],infact[MAXN];
-
-ll q_pow(ll a,ll b,int m) {
-    ll res = 1;
-    while (b) {
-        if (b & 1) {
-            res = res * a % m;
+struct Comb {
+    int n;
+    vector<ll> fac;
+    vector<ll> invfac;
+    Comb(){}
+    Comb(int n_) {
+        init(n_);
+    }
+    void init(int n_) {
+        n = n_;
+        fac.assign(n + 1, 1);
+        invfac.assign(n + 1, 1);
+        for (int i = 1; i <= n; i++) {
+            fac[i] = fac[i - 1] * i % mod;
+            invfac[i] = invfac[i - 1] * q_pow(i, mod - 2, mod) % mod;
         }
-        b >>= 1;
-        a = a * a % m;
     }
-    return res;
-}
-
-void init() {
-    fact[0] = infact[0] = 1;
-    for (int i = 1; i < MAXN; i++) {
-        fact[i] = fact[i - 1] * i % mod;
-        infact[i] = infact[i - 1] * q_pow(i, mod - 2, mod) % mod;
+    ll comb(int a, int b) {
+        return fac[a] * invfac[a - b] % mod * invfac[b] % mod;
     }
-}
-
-ll comb(int a,int b) {
-    return fact[a] * infact[a - b] % mod * infact[b] % mod;
-}
+};
