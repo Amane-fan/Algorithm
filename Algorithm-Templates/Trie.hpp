dc7fcd5531eeea66ad98b8cb622ba3ee0cc940c6
Amane <3319682713@qq.com>
2025-01-08 19:40:36 +0800

    update Trie template

--- Algorithm-Templates/Trie.hpp
+++ Algorithm-Templates/Trie.hpp
@@ -1,49 +1,44 @@
-template<class Node>
 struct Trie {
-    Node* root;
+    struct Node {
+        int cnt;
+        array<int,26> ch;
+        Node(): cnt(0), ch{} {}
+    };
+    vector<Node> t;
     Trie() {
-        root = new Node();
+        init();
     }
-    ~Trie() {
-        deleteTree(root);
+    void init() {
+       newNode();
     }
-    void deleteTree(Node* node) {
-        if (node == nullptr) return;
-        for (auto &e : node->trie) {
-            deleteTree(e);
-        }
-        delete node;
-    }
-    void insert(string s) {
-        int n = s.size();
-        Node* p = root;
-        for (int i = 0; i < n; i++) {
-            int u = get(s[i]);
-            if (p->trie[u] == nullptr) p->trie[u] = new Node();
-            p = p->trie[u];
-        }
-        p->cnt++;
-    }
-    Node* query(string s) {
-        int n = s.size();
-        Node* p = root;
-        for (int i = 0; i < n; i++) {
-            int u = get(s[i]);
-            if (p->trie[u] == nullptr) return nullptr;
-            p = p->trie[u];
-        }
-        return p;
+    int newNode() {
+        t.push_back(Node());
+        return t.size() - 1;
     }
     int get(char c) {
         return c - 'a';
     }
-};
-
-struct Node {
-    int cnt;
-    int ans;
-    array<Node*, 26> trie;
-    Node():cnt(0),ans(0) {
-        fill(trie.begin(), trie.end(), nullptr);
+    void insert(const string &s) {
+        int n = s.size();
+        int p = 0;
+        for (int i = 0; i < n; i++) {
+            int u = get(s[i]);
+            if (t[p].ch[u] == 0) {
+                int k = newNode();
+                t[p].ch[u] = k;
+            }
+            p = t[p].ch[u];
+        }
+        t[p].cnt++;
+    }
+    Node query(const string &s) {
+        int n = s.size();
+        int p = 0;
+        for (int i = 0; i < n; i++) {
+            int u = get(s[i]);
+            if (t[p].ch[u] == 0) return Node();
+            p = t[p].ch[u];
+        }
+        return t[p];
     }
 };
