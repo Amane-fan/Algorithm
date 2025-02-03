f2b77f0b9cfbc88776e9f1d5ec1b857e801f4b40
Amane <3319682713@qq.com>
2025-02-03 15:59:43 +0800

    2025/2/3 update Comb template

--- Algorithm-Templates/Math/Comb.hpp
+++ Algorithm-Templates/Math/Comb.hpp
@@ -1,21 +1,58 @@
+ll power(ll a, ll b, const int m) {
+    ll res = 1;
+    while (b) {
+        if (b & 1) res = res * a % m;
+        b >>= 1;
+        a = a * a % m;
+    }
+    return res;
+}
+
+constexpr int mod = 1e9 + 7;
+
 struct Comb {
     int n;
-    vector<ll> fac;
-    vector<ll> invfac;
-    Comb(){}
-    Comb(int n_) {
-        init(n_);
+    vector<ll> _fac, _infac, _inv;
+    Comb(): n{0}, _fac{1}, _infac{1}, _inv{0} {}
+    Comb(int m): Comb() {
+        init(m);
     }
-    void init(int n_) {
-        n = n_;
-        fac.assign(n + 1, 1);
-        invfac.assign(n + 1, 1);
-        for (int i = 1; i <= n; i++) {
-            fac[i] = fac[i - 1] * i % mod;
-            invfac[i] = invfac[i - 1] * q_pow(i, mod - 2, mod) % mod;
+    void init(int m) {
+        if (m <= n) return;
+        _fac.resize(m + 1);
+        _infac.resize(m + 1);
+        _inv.resize(m + 1);
+        for (int i = n + 1; i <= m; i++) {
+            _fac[i] = _fac[i - 1] * i % mod;
         }
+        _infac[m] = power(_fac[m], mod - 2, mod);
+        for (int i = m; i > n; i--) {
+            _infac[i - 1] = _infac[i] * i % mod;
+            _inv[i] = _infac[i] * _fac[i - 1] % mod;
+        }
+        n = m;
     }
-    ll comb(int a, int b) {
-        return fac[a] * invfac[a - b] % mod * invfac[b] % mod;
+    ll fac(int m) {
+        if (m > n) init(m * 2);
+        return _fac[m];
     }
-};
+    ll infac(int m) {
+        if (m > n) init(m * 2);
+        return _infac[m];
+    }
+    ll inv(int m) {
+        if (m > n) init(m * 2);
+        return _inv[m];
+    }
+    ll C(int a, int b) {
+        if (a < b || b < 0) return 0ll;
+        return fac(a) * infac(a - b) % mod * infac(b) % mod;
+    }
+    ll A(int a, int b) {
+        if (a < b || b < 0) return 0ll;
+        return fac(a) * infac(a - b) % mod;
+    }
+    ll A(int a) {
+        return fac(a);
+    }
+} comb;
