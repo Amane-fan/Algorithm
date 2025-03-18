15080c8f78f7f2f0631594790a6cc94102b553a8
Amane <3319682713@qq.com>
2025-03-18 19:38:39 +0800

    2025/3/18 update super StrHash template

--- Algorithm-Templates/String/StrHash.hpp
+++ Algorithm-Templates/String/StrHash.hpp
@@ -1,41 +1,37 @@
-bool isPrime(int n) {
-    if (n <= 1) return false;
-    for (int i = 2; i <= n / i; i++) {
-        if (n % i == 0) return false;
-    }
-    return true;
-}
-
-int findPrime(int n) {
-    while (!isPrime(n)) {
-        n++;
-    }
-    return n;
-}
-
-std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());
-const int P = findPrime(rng() % 900'000'000 + 900'000'000),
-base = std::uniform_int_distribution<int>(8e8, 9e8)(rng);
+using ull = unsigned long long;
+constexpr ull mod = (1ull << 61) - 1;
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<ull> dist(mod / 2, mod - 1);
+const ull base = dist(rnd);
 
 struct Hash {
-    vector<ll> h;
-    vector<ll> p;
-    Hash(){}
-    Hash(string &s) {
+    vector<ull> h;
+    vector<ull> p;
+    Hash() {}
+    Hash(const string &s) {
         init(s);
     }
-    void init(string &s) {
-        int n = s.size();
-        s = " " + s;
+    ull add(ull a, ull b) {
+        a += b;
+        if (a >= mod) a -= mod;
+        return a;
+    }
+    ull mul(ull a, ull b) {
+        __uint128_t c = __uint128_t(a) * b;
+        return add(c >> 61, c & mod);
+    }
+    void init(const string &s) {
+        int n = s.size() - 1;
         p.resize(n + 1);
         h.resize(n + 1);
         p[0] = 1;
         for (int i = 1; i <= n; i++) {
-            p[i] = p[i - 1] * base % P;
-            h[i] = (h[i - 1] * base + s[i]) % P;
+            p[i] = mul(p[i - 1], base);
+            h[i] = mul(h[i - 1], base);
+            h[i] = add(h[i], s[i]);
         }
     }
-    ll get(int l, int r) {
-        return (h[r] - h[l - 1] * p[r - l + 1] % P + P) % P;
+    ull get(int l, int r) {
+        return add(h[r], mod - mul(h[l - 1], p[r - l + 1]));
     } 
 };
