54f884699b80f1a19ec054d6e93fc5b114cc0012
Amane <3319682713@qq.com>
2025-03-22 11:00:03 +0800

    2025/3/22 update Mint template

--- Algorithm-Templates/DS/MInt.hpp
+++ Algorithm-Templates/DS/MInt.hpp
@@ -1,8 +1,4 @@
-/**   取模类（MLong & MInt 新版）
- *    2023-08-14: https://ac.nowcoder.com/acm/contest/view-submission?submissionId=63433564
- *
- *    根据输入内容动态修改 MOD 的方法：Z::setMod(p) 。
-**/
+using i64 = long long;
 template<class T>
 constexpr T power(T a, i64 b) {
     T res = 1;
@@ -22,101 +18,6 @@ constexpr i64 mul(i64 a, i64 b, i64 p) {
     }
     return res;
 }
-template<i64 P>
-struct MLong {
-    i64 x;
-    constexpr MLong() : x{} {}
-    constexpr MLong(i64 x) : x{norm(x % getMod())} {}
-    
-    static i64 Mod;
-    constexpr static i64 getMod() {
-        if (P > 0) {
-            return P;
-        } else {
-            return Mod;
-        }
-    }
-    constexpr static void setMod(i64 Mod_) {
-        Mod = Mod_;
-    }
-    constexpr i64 norm(i64 x) const {
-        if (x < 0) {
-            x += getMod();
-        }
-        if (x >= getMod()) {
-            x -= getMod();
-        }
-        return x;
-    }
-    constexpr i64 val() const {
-        return x;
-    }
-    explicit constexpr operator i64() const {
-        return x;
-    }
-    constexpr MLong operator-() const {
-        MLong res;
-        res.x = norm(getMod() - x);
-        return res;
-    }
-    constexpr MLong inv() const {
-        assert(x != 0);
-        return power(*this, getMod() - 2);
-    }
-    constexpr MLong &operator*=(MLong rhs) & {
-        x = mul(x, rhs.x, getMod());
-        return *this;
-    }
-    constexpr MLong &operator+=(MLong rhs) & {
-        x = norm(x + rhs.x);
-        return *this;
-    }
-    constexpr MLong &operator-=(MLong rhs) & {
-        x = norm(x - rhs.x);
-        return *this;
-    }
-    constexpr MLong &operator/=(MLong rhs) & {
-        return *this *= rhs.inv();
-    }
-    friend constexpr MLong operator*(MLong lhs, MLong rhs) {
-        MLong res = lhs;
-        res *= rhs;
-        return res;
-    }
-    friend constexpr MLong operator+(MLong lhs, MLong rhs) {
-        MLong res = lhs;
-        res += rhs;
-        return res;
-    }
-    friend constexpr MLong operator-(MLong lhs, MLong rhs) {
-        MLong res = lhs;
-        res -= rhs;
-        return res;
-    }
-    friend constexpr MLong operator/(MLong lhs, MLong rhs) {
-        MLong res = lhs;
-        res /= rhs;
-        return res;
-    }
-    friend constexpr std::istream &operator>>(std::istream &is, MLong &a) {
-        i64 v;
-        is >> v;
-        a = MLong(v);
-        return is;
-    }
-    friend constexpr std::ostream &operator<<(std::ostream &os, const MLong &a) {
-        return os << a.val();
-    }
-    friend constexpr bool operator==(MLong lhs, MLong rhs) {
-        return lhs.val() == rhs.val();
-    }
-    friend constexpr bool operator!=(MLong lhs, MLong rhs) {
-        return lhs.val() != rhs.val();
-    }
-};
-
-template<>
-i64 MLong<0LL>::Mod = i64(1E18) + 9;
 
 template<int P>
 struct MInt {
@@ -211,11 +112,5 @@ struct MInt {
     }
 };
 
-template<>
-int MInt<0>::Mod = 998244353;
-
-template<int V, int P>
-constexpr MInt<P> CInv = MInt<P>(V).inv();
-
-constexpr int P = 1000000007;
+constexpr int P = 998244353;
 using Z = MInt<P>;
